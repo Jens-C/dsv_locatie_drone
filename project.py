26bb109b67712454bb1bf174f32d55b698e1065e
jens <jenscocquyt@gmail.com>
2023-11-24 12:37:55 +0100

    fixed channel2ADPD

--- project.py
+++ project.py
@@ -9,14 +9,20 @@ import scipy
 import matplotlib.pyplot as plt
 
 def channel2APDP(positie):
-    num_positions = positie.shape[0]  
+    num_positions =  positie.shape[1]
     APDP_list = []
+    APDP_list = []
+    PDP=np.zeros((100,200))
     print(num_positions)
-    for i in range(num_positions):
-        impulsrespons = np.abs(scipy.fftpack.ifft(positie[i, :]))
-        PDP = np.sum(np.square(impulsrespons)) / len(impulsrespons)
-        APDP_list.append(PDP)
 
+    for i in range(num_positions):
+
+        impulsrespons = np.abs(scipy.fft.ifft(positie[:, i]))
+        PDP[i] = np.square(impulsrespons)
+
+    for i in range(200):
+        print(len(PDP[:,i]))
+        APDP_list.append(np.sum(PDP[:,i])/len(PDP[:,i]))
     return APDP_list
 def APDP2delays(APDP):
     # zoek de pieken
@@ -29,13 +35,11 @@ mat_contents = sio.loadmat('Dataset_1.mat')
 matrix = mat_contents['H']   #200 frequentietonen, 25 posities, 100 kopies voor foutmeting
 
 
-APDP_values = channel2APDP(matrix[:,5,:])
+APDP_values = channel2APDP(matrix[:,1,:])
 val0, val1 = APDP2delays(APDP_values)
 
 print( val0)
 print(val1)
-
-
 
 plt.plot(APDP_values)
 
